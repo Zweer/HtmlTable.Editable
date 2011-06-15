0102c54f7271eaf0a9eb9d196b2561fa2697d698
Niccolo' Olivieri <flicofloc@gmail.com>
2011-06-15 19:14:20 +0200

    Added the first structure of the project

---
+++ Source/HtmlTable.Editable.js
@@ -0,0 +1,27 @@
+/*
+---
+description: makes an HtmlTable's cells editable and submits the result to a server page
+
+license: MIT-style
+
+authors: Niccolò Olivieri
+
+requires: [Core, More]
+
+provides: HtmlTable.Editable
+
+...
+*/
+
+HtmlTable.Editable = new Class({
+    Extends: HtmlTable,
+
+    options: {
+
+    },
+
+    initialize: function()
+    {
+        
+    }
+});
